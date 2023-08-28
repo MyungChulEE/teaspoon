c9b21cb4c1603eabf7a6c631961568ad61204bfc
suyeon0000 <suyeon011@gmail.com>
2023-08-28 13:04:48 +0900

    chore: 'init'

---
+++ teamall/src/main/java/com/teaspoon/teamall/TeamallApplication.java
@@ -0,0 +1,13 @@
+package com.teaspoon.teamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamallApplication.class, args);
+	}
+
+}
---
+++ teamall/src/test/java/com/teaspoon/teamall/TeamallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teaspoon.teamall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
