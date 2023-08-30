c2eb9d66478a44926de576057c8f3427d0796275
suyeon0000 <suyeon011@gmail.com>
2023-08-30 16:07:52 +0900

    commit

---
+++ teamall/src/main/java/com/teaspoon/teamall/domain/product/dto/ProductDTO.java
@@ -0,0 +1,22 @@
+package com.teaspoon.teamall.domain.product.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private int productNo;
+    private int prodCatePk;
+    private String productName;
+    private String productContent;
+    private int productPrice;
+    private int productCnt;
+
+
+
+
+}
